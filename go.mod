3451ae3aaa5c3eec6df003afba0ee3d4a4b33812
Ayanrocks <ayanb1999@gmail.com>
2020-01-15 16:16:50 +0530

    Added auth service through http

--- go.mod
+++ go.mod
@@ -5,8 +5,8 @@ go 1.12
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/go-redis/redis v6.15.6+incompatible
-	golang.org/x/lint v0.0.0-20190313153728-d0100b6bd8b3 // indirect
-	golang.org/x/tools v0.0.0-20190524140312-2c0ae7006135 // indirect
+	github.com/golang/protobuf v1.3.2
+	github.com/onsi/ginkgo v1.11.0 // indirect
+	github.com/onsi/gomega v1.8.1 // indirect
 	google.golang.org/grpc v1.26.0
-	honnef.co/go/tools v0.0.0-20190523083050-ea95bdfd59fc // indirect
 )
--- grpc-auth-api/server/main.go
+++ grpc-auth-api/server/main.go
@@ -1,116 +1,100 @@
 package main
 
 import (
-	"context"
-	"errors"
+	// "context"
+	// "errors"
 	"fmt"
-	"log"
-	"net"
-
-	"grpc-auth-api/services"
-	"github.com/dgrijalva/jwt-go"
-	"github.com/go-redis/redis"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
+	"github.com/gin-gonic/gin"
+	"grpc-auth-api/server/controllers"
+	"grpc-auth-api/server/database"
+	// "log"
+	// "net"
 )
 
-type server struct{}
+// type server struct{}
 
-var jwtKey = []byte("qwerty")
+// var jwtKey = []byte("qwerty")
 
-type Claims struct {
-	Id string `json:"Id"`
-	jwt.StandardClaims
-}
+// type Claims struct {
+// 	Id string `json:"Id"`
+// 	jwt.StandardClaims
+// }
 
-var url, _ = redis.ParseURL("redis://redis:6379")
-var client = redis.NewClient(&redis.Options{
-	Addr:     url.Addr,
-	Password: "", // no password set
-	DB:       0,  // use default DB
-})
+// var url, _ = redis.ParseURL("redis://redis:6379")
+// var client = redis.NewClient(&redis.Options{
+// 	Addr:     url.Addr,
+// 	Password: "", // no password set
+// 	DB:       0,  // use default DB
+// })
 
-func SaveToDB(token string, machId string) {
+// func (s *server) GenToken(ctx context.Context, request *protos.Request) (*protos.Response, error) {
+// 	machineID := request.GetMachineId()
+// 	claims := &Claims{
+// 		Id:             machineID,
+// 		StandardClaims: jwt.StandardClaims{},
+// 	}
+// 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+// 	tokenString, err := token.SignedString(jwtKey)
+// SaveToDB(tokenString, machineID)
 
-	pong, err := client.Ping().Result()
-	fmt.Println(pong, err)
-	client.Set(machId, token, 0)
-	if err != nil {
-		log.Fatal(err)
-	}
-	// Output: PONG <nil>
-}
+// 	return &protos.Response{Token: tokenString}, err
+// }
 
-func CheckFromDB(token string, machId string) bool {
-	var ret bool
-	// client := redis.NewClient(&redis.Options{
-	// 	Addr:     "redis:6379",
-	// 	Password: "", // no password set
-	// 	DB:       0,  // use default DB
-	// })
-	val, err := client.Get(machId).Result()
-	if err != nil {
-		ret = false
-	}
-	if val == token {
-		ret = true
-	} else {
-		ret = false
-	}
-	return ret
-}
+// func (s *server) VerifyToken(ctx context.Context, request *protos.VerifyRequest) (*protos.VerifyResponse, error) {
+// 	var msg string
+// 	var err error
+// 	Token := request.GetToken()
+// 	tokenString, err := jwt.Parse(Token, func(token *jwt.Token) (interface{}, error) {
+// 		// Don't forget to validate the alg is what you expect:
+// 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+// 			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+// 		} else {
+// 			return jwtKey, nil
+// 		}
+// 	})
+// 	if claims, ok := tokenString.Claims.(jwt.MapClaims); ok && tokenString.Valid {
+// 		machineID := claims["Id"].(string)
+// 		resp := CheckFromDB(Token, machineID)
+// 		if resp {
+// 			msg = "Verified!!!"
+// 		} else {
+// 			msg = "Token Invalid!!!"
+// 			err = errors.New("Invalid Machine id")
+// 		}
+// 	} else {
+// 		msg = "Token Invalid!!!"
+// 		err = errors.New("Invalid JWT Token")
+// 	}
+// 	return &protos.VerifyResponse{Msg: msg}, err
+// }
 
-func (s *server) GenToken(ctx context.Context, request *protos.Request) (*protos.Response, error) {
-	machineID := request.GetMachineId()
-	claims := &Claims{
-		Id:             machineID,
-		StandardClaims: jwt.StandardClaims{},
-	}
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	tokenString, err := token.SignedString(jwtKey)
-	SaveToDB(tokenString, machineID)
+// func main() {
+// 	listener, err := net.Listen("tcp", ":4040")
+// 	if err != nil {
+// 		panic(err)
+// 	}
 
-	return &protos.Response{Token: tokenString}, err
-}
-
-func (s *server) VerifyToken(ctx context.Context, request *protos.VerifyRequest) (*protos.VerifyResponse, error) {
-	var msg string
-	var err error
-	Token := request.GetToken()
-	tokenString, err := jwt.Parse(Token, func(token *jwt.Token) (interface{}, error) {
-		// Don't forget to validate the alg is what you expect:
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
-		} else {
-			return jwtKey, nil
-		}
-	})
-	if claims, ok := tokenString.Claims.(jwt.MapClaims); ok && tokenString.Valid {
-		machineID := claims["Id"].(string)
-		resp := CheckFromDB(Token, machineID)
-		if resp {
-			msg = "Verified!!!"
-		} else {
-			msg = "Token Invalid!!!"
-			err = errors.New("Invalid Machine id")
-		}
-	} else {
-		msg = "Token Invalid!!!"
-		err = errors.New("Invalid JWT Token")
-	}
-	return &protos.VerifyResponse{Msg: msg}, err
-}
-
+// 	srv := grpc.NewServer()
+// 	fmt.Println("GRPC Server Started")
+// 	protos.RegisterGenJWTTokenServer(srv, &server{})
+// 	reflection.Register(srv)
+// 	if e := srv.Serve(listener); e != nil {
+// 		panic(err)
+// 	}
+// }
 func main() {
-	listener, err := net.Listen("tcp", ":4040")
+	r := gin.Default()
+
+	pong, err := database.Client.Ping().Result()
+
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
 	}
 
-	srv := grpc.NewServer()
-	protos.RegisterGenJWTTokenServer(srv, &server{})
-	reflection.Register(srv)
-	if e := srv.Serve(listener); e != nil {
-		panic(err)
-	}
+	fmt.Println(pong)
+
+	r.GET("/getToken/:machineID", controllers.GetToken)
+	r.GET("/verifyToken/:token", controllers.VerifyToken)
+
+	r.Run()
 }
